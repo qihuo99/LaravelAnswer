1ac46ce7fb7586efb7b36f1a881b6ff1cde0fc42
juchu168 <qihuo99@users.noreply.github.com>
2020-06-26 20:20:22 +0800

    Complete the Questions Controller create questions and view functions, tested to run OK.

---
+++ app/Question.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+//the below line bring in all the lib and functions a model needs for working properly
+//everything is imported in one single file
+use Illuminate\Database\Eloquent\Model;
+
+//this model will look for a table named Questions, since model is singular, 
+//so table name will be plural
+class Question extends Model
+{
+    //
+
+
+}
